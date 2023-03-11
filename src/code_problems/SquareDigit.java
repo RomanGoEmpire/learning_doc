f980359d6b9dc9f3db1a45bee66d0d1996540393
RomanGoEmpire <gerloffroman@gmail.com>
2023-03-11 13:39:29 +0100

    space

--- src/code_problems/SquareDigit.java
+++ src/code_problems/SquareDigit.java
@@ -11,6 +11,7 @@ For example, if we run 9119 through the function, 811181 will come out, because 
 Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
 
 Note: The function accepts an integer and returns an integer.
+
 Happy Coding!
  */
 
